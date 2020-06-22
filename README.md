16be90c6eaf8bccd67ecd465827567e92a20610d
Nick McIntyre <nick@mcintyre.io>
2020-06-22 18:32:19 -0500

    pframe -> p5.frame

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# pframe
+# p5.frame
 > A virtual reality library for p5.js built on A-Frame
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'pframe.js',
+    filename: 'p5.frame.js',
     path: path.resolve(__dirname, 'dist'),
   },
   externals: {
